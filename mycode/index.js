ce752ff8837d7c1a6c11648fb986a6108b79d71e
Jose Luis Gimenez <joseviedmacorreo@gmail.com>
2023-10-02 21:43:21 -0300

    cambio todo ok

--- mycode/index.js
+++ mycode/index.js
@@ -1,15 +1,9 @@
 
 function hello() {
     setTimeout(() => {
-        
-        Boolean sondeamos = "consulta a base de datos tabla cofig celda sondeo";
-
-        if (sondeamos) {
-            client.publish("topico de sondeo", "mensaje de sondeo");
-        } else {
-            console.log("no hacemos nada");
-        }
+        console.log("hello from docker node");
         hello();
     }, 1000);
 }
+
 hello();
